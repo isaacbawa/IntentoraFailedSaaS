0e4ca79428875d4fdc4cd58c2f992f2d408888ec
Isaac Bawa Ngisah <isaacbawan@gmail.com>
2025-12-02 00:30:43 +0000

    Delete src directory

--- src/utils/dataStore.ts
+++
@@ -1,7 +0,0 @@
-// Legacy DataStore - replaced by SupabaseDataStore
-// This file is kept for backward compatibility during migration
-import SupabaseDataStore from './supabaseDataStore';
-
-// Re-export types and instance for backward compatibility
-export { Teardown, SubmittedStory } from './supabaseDataStore';
-export default SupabaseDataStore;
